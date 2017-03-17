60876e12b218f1c0e539742332f8e369f874505a
Leonard Luis and Sean Peterson <leonard@>
2017-03-16 17:57:45 -0700

    add object constructor and prototype method to look up doctors

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,13 @@
-function Object(parameter) {
-  this.property = parameter;
+function Doctor() {
 }
 
-Object.prototype.someFunction = function(n1, n2) {
-  return n1 + n2;
+Doctor.prototype.lookup = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+
 };
