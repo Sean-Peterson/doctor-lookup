51e088ea1a1fc97a197e9d8feeef60d14c4b7164
Leonard Luis and Sean Peterson <sean@>
2017-03-16 18:21:27 -0700

    add variables equal to information that I want to pass into my call back function

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -10,5 +10,5 @@ var results = function(name, number, address, city, state) {
 $(document).ready(function(){
   var doctor = new Doctor();
   var medicalIssue = 'stomach ache';
-  doctor.lookup(medicalIssue);
+  doctor.lookup(medicalIssue, results);
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,10 +3,16 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.lookup = function(medicalIssue) {
+Doctor.prototype.lookup = function(medicalIssue, results) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
-      console.log(response);
+     console.log(response);
+     var name = response.data[0].practices[0].name;
+     console.log(name);
+     var number = response.data[0].practices[0].phones[0].number;
+     var address = response.data[0].practices[0].visit_address.street;
+     var city = response.data[0].practices[0].visit_address.city;
+     var state = response.data[0].practices[0].visit_address.state;
     })
    .fail(function(error){
       console.log("fail");
