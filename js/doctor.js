14d2f69cc10f09eec14e9294d879596ae43ae3b3
Leonard Luis and Sean Peterson <leonard@>
2017-03-16 18:42:45 -0700

    add empty arrays for names, numbers, addresses, cities, and states and push the information to those arrays

--- js/doctor.js
+++ js/doctor.js
@@ -7,18 +7,26 @@ Doctor.prototype.lookup = function(medicalIssue, results) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(response) {
       console.log(response);
-      var doctors = [];
+
+      var names = [];
+      var numbers = [];
+      var addresses = [];
+      var cities = [];
+      var states = [];
+
       var length = response.data.length;
-      console.log(length);
       for(i=0;i<response.data.length;i++){
        var name = response.data[i].practices[0].name;
+       names.push(name)
        var number = response.data[i].practices[0].phones[0].number;
+       numbers.push(number)
        var address = response.data[i].practices[0].visit_address.street;
+       addresses.push(address)
        var city = response.data[i].practices[0].visit_address.city;
+       cities.push(city)
        var state = response.data[i].practices[0].visit_address.state;
-       doctors.push(name, number, address, city, state);
+       states.push(state);
       }
-      console.log(doctors);
     })
     .fail(function(error){
       console.log("fail");
