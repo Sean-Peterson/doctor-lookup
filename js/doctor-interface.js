6baa5fe2bb027cabc37be3c8a0b473a9989555d5
Leonard Luis and Sean Peterson <sean@>
2017-03-16 19:03:16 -0700

    add header and form in html

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,7 +2,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var results = function(names, numbers, addresses, cities, states) {
   console.log(names);
-
+  $('#doctor-list').empty();
   for(i=0;i<names.length;i++){
     $('#doctor-list').append("<div class='doctor"+i+"'><ul><li>Name: " + names[i] + "</li><li>Number: " + numbers[i] + "</li><li>Address: " + addresses[i] +", " + cities[i] + ", " + states[i] + "</li>             </ul></div>")
     console.log('balls');
