4bd9f74fa38b3f844a57902c5972787a165f1695
Leonard Luis and Sean Peterson <leonard@>
2017-03-16 19:35:41 -0700

    add styling

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,10 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var results = function(names, numbers, addresses, cities, states) {
-  console.log(names);
   $('#doctor-list').empty();
   for(i=0;i<names.length;i++){
-    $('#doctor-list').append("<div class='doctor"+i+"'><ul><li>Name: " + names[i] + "</li><li>Number: " + numbers[i] + "</li><li>Address: " + addresses[i] +", " + cities[i] + ", " + states[i] + "</li>             </ul></div><br>")
-    console.log('balls');
-  };
+    $('#doctor-list').append("<div class='doctor-results' id='doctor"+i+"'><ul id='doctor-list'><li>Name: " + names[i] + "</li><li>Number: " + numbers[i] + "</li><li>Address: " + addresses[i] +", " + cities[i] + ", " + states[i] + "</li></ul></div><br>");
+  }
 };
 
 
--- js/doctor.js
+++ js/doctor.js
@@ -6,7 +6,6 @@ function Doctor() {
 Doctor.prototype.lookup = function(medicalIssue, results) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(response) {
-      console.log(response);
 
       var names = [];
       var numbers = [];
@@ -17,13 +16,13 @@ Doctor.prototype.lookup = function(medicalIssue, results) {
       var length = response.data.length;
       for(i=0;i<response.data.length;i++){
        var name = response.data[i].practices[0].name;
-       names.push(name)
+       names.push(name);
        var number = response.data[i].practices[0].phones[0].number;
-       numbers.push(number)
+       numbers.push(number);
        var address = response.data[i].practices[0].visit_address.street;
-       addresses.push(address)
+       addresses.push(address);
        var city = response.data[i].practices[0].visit_address.city;
-       cities.push(city)
+       cities.push(city);
        var state = response.data[i].practices[0].visit_address.state;
        states.push(state);
       }
